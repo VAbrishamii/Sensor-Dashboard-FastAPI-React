9cd96eb67bd8f12638c8b07fbc9cd1079b59ac7d
VAbrishamii <vahide.abri@yahoo.com>
2025-09-17 14:46:34 +0200

    Create api/sensors.ts

---
+++ frontend/src/api/sensors.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+import type { SensorData } from "../interfaces/Sensor";
+
+const API_BASE = "http://127.0.0.1:8000";
+
+export const getSensors = async (): Promise<SensorData[]> => {
+  const res = await axios.get<SensorData[]>(`${API_BASE}/sensors`);
+  return res.data;
+};
+
+export const getSensorById = async (id: number): Promise<SensorData> => {
+  const res = await axios.get<SensorData>(`${API_BASE}/sensors/${id}`);
+  return res.data;
+};
