db69e52499a974128d5e049e0b8ee04271aedbb8
VAbrishamii <vahide.abri@yahoo.com>
2025-09-17 12:07:47 +0200

    Models difinition

---
+++ backend/models.py
@@ -0,0 +1,18 @@
+# models.py 
+from pydantic import BaseModel
+from typing import List, Optional
+from datetime import datetime
+
+# represent a sensor reading
+class SensorReading(BaseModel):
+    timestamp: datetime
+    temerature: float
+    humidity: float
+    pressure: float
+# represent a sensor with its metadata and readings
+class SensorData(BaseModel):
+    id: int
+    name: str
+    location: str
+    description: Optional[str] = None
+    readings: List[SensorReading] = []
