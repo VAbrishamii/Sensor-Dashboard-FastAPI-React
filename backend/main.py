2a16e011076a5f399d387478de18ba62d8506d12
VAbrishamii <vahide.abri@yahoo.com>
2025-09-22 16:13:17 +0200

    Update origins in main.py

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ import random
 app = FastAPI(title="Sensor Data API", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174"],
+    allow_origins=["http://localhost:5173", "http://localhost:5174", "https://sensor-dashboard-fast-api-react.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
