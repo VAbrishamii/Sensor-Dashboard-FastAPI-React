682e01762cf9e86ef2771fc5b621550038b75bc7
VAbrishamii <vahide.abri@yahoo.com>
2025-09-22 16:06:59 +0200

    Update API_BASE

--- frontend/src/api/sensors.ts
+++ frontend/src/api/sensors.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { SensorData } from "../interfaces/Sensor";
 
-export const API_BASE = "http://127.0.0.1:8000";
+export const API_BASE = "https://sensor-dashboard-fastapi-react.onrender.com/";
 
 export const getSensors = async (): Promise<SensorData[]> => {
   const res = await axios.get<SensorData[]>(`${API_BASE}/sensors`);
